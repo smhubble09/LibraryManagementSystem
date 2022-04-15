c310d0c437fb7698253d56aa88e12e3af62cfbb3
shane.hubble <smhubble09@gmail.com>
2022-04-15 15:51:20 -0600

    Add project files.

---
+++ StartForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace LibraryManagementSystem {
+    partial class StartForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.LibrarianButton = new System.Windows.Forms.Button();
+            this.UserButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.UsernameInput = new System.Windows.Forms.TextBox();
+            this.PasswordInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LibrarianButton
+            // 
+            this.LibrarianButton.Location = new System.Drawing.Point(11, 80);
+            this.LibrarianButton.Name = "LibrarianButton";
+            this.LibrarianButton.Size = new System.Drawing.Size(142, 35);
+            this.LibrarianButton.TabIndex = 0;
+            this.LibrarianButton.Text = "Librarian Login";
+            this.LibrarianButton.UseVisualStyleBackColor = true;
+            this.LibrarianButton.Click += new System.EventHandler(this.LibrarianButton_Click);
+            // 
+            // UserButton
+            // 
+            this.UserButton.Location = new System.Drawing.Point(159, 80);
+            this.UserButton.Name = "UserButton";
+            this.UserButton.Size = new System.Drawing.Size(142, 35);
+            this.UserButton.TabIndex = 1;
+            this.UserButton.Text = "User Login";
+            this.UserButton.UseVisualStyleBackColor = true;
+            this.UserButton.Click += new System.EventHandler(this.UserButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(307, 80);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(142, 35);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // UsernameInput
+            // 
+            this.UsernameInput.Location = new System.Drawing.Point(131, 12);
+            this.UsernameInput.Name = "UsernameInput";
+            this.UsernameInput.Size = new System.Drawing.Size(217, 20);
+            this.UsernameInput.TabIndex = 3;
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.Location = new System.Drawing.Point(131, 41);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.Size = new System.Drawing.Size(217, 20);
+            this.PasswordInput.TabIndex = 4;
+            this.PasswordInput.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 121);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.UsernameInput);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.UserButton);
+            this.Controls.Add(this.LibrarianButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StartForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Book Checkout";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LibrarianButton;
+        private System.Windows.Forms.Button UserButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.TextBox UsernameInput;
+        private System.Windows.Forms.TextBox PasswordInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ StartForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystem {
+    public partial class StartForm : Form {
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=C:\repos\noteTaker\NotesApp.accdb; Jet OLEDB:Database Password=libraryApp22!";
+        public StartForm() {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void UserButton_Click(object sender, EventArgs e) {
+            if (UsernameInput.Text == "") {
+                DialogResult dialog = MessageBox.Show("Please provide a username.", "Missing Field", MessageBoxButtons.OK);
+            }
+            else if (PasswordInput.Text == "") {
+                DialogResult dialog = MessageBox.Show("Please provide a password.", "Missing Field", MessageBoxButtons.OK);
+            }
+            else {
+                var userForm = new UserForm();
+                userForm.Show();
+                this.Hide();
+            }
+        }
+
+        private void LibrarianButton_Click(object sender, EventArgs e) {
+            if (UsernameInput.Text == "") {
+                DialogResult dialog = MessageBox.Show("Please provide a username.", "Missing Field", MessageBoxButtons.OK);
+            }
+            else if (PasswordInput.Text == "") {
+                DialogResult dialog = MessageBox.Show("Please provide a password.", "Missing Field", MessageBoxButtons.OK);
+            }
+            else {
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                string query = "select * from Users where username like @username and password = @password;";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", UsernameInput.Text);
+                cmd.Parameters.AddWithValue("@password", PasswordInput.Text);
+
+                conn.Open();
+                DataSet ds = new DataSet();
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                da.Fill(ds);
+                conn.Close();
+
+                bool loginSuccessful = ((ds.Tables.Count > 0) && (ds.Tables[0].Rows.Count > 0));
+
+                if (loginSuccessful) {
+                    var librarianForm = new LibrarianForm();
+                    librarianForm.Show();
+                    this.Hide();
+                }
+                else {
+                    DialogResult dialog = MessageBox.Show("Invalid username/password.", "Invalid login", MessageBoxButtons.OK);
+                }
+                    
+            }   
+        }
+    }
+}
